f6456c74e7c58ca396d196152fdae58f8c17db93
yatmayaka <student.tm@youngsvilleacademy.org>
2026-04-23 13:15:13 -0400

    Create dark-mode-toggle.js

---
+++ wdsub/dark-mode-toggle.js
@@ -0,0 +1,21 @@
+let topDiv = document.getElementById("top-div");
+let darkOff = document.getElementById("dark-off");
+
+let isLight = true;
+function mngMode(event) {
+
+
+// Change background color to black if light mode, otherwise its white
+if (isLight) document.body.style.backgroundColor ="black";
+  else document.body.style.backgroundColor ="white"; 
+//change text color to white if else black
+if (isLight) document.body.style.Color ="white" ;
+  else document.body.style.Color ="black" ;
+// Change the text to dark mode on" if light mode else "dark mode off" 
+if (isLight) darkOff.innerHTML = "<p>Dark mode off</p>";
+  else darkOff.innerHTML = "<p>Dark mode off</p>";
+  
+//flip the is light switch
+  isLight = !isLight;
+
+}
