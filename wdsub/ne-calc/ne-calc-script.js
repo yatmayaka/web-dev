f87a332565fee782f20eab91dc59532e099fc7e2
yatmayaka <student.tm@youngsvilleacademy.org>
2026-06-01 16:52:15 -0400

    Update ne-calc-script.js

--- wdsub/ne-calc/ne-calc-script.js
+++ wdsub/ne-calc/ne-calc-script.js
@@ -2,8 +2,8 @@
 
 let urlParams = new URLSearchParams(window.location.search);
 
-const P1_STRATS = Number(urlParams.get("p1Strats"));
-const P2_STRATS = Number(urlParams.get("p2Strats"));
+const P1_NUM_STRATS = Number(urlParams.get("p1Strats"));
+const P2_NUM_STRATS = Number(urlParams.get("p2Strats"));
 
 const PAYOFF_CONTENTS = "(<input type='number'>,<input type='number'>)";
 
@@ -111,5 +111,5 @@ function compute() {
     }
   }
 }
-  
+
   
